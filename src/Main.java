acfe3899f5094ce7f046a5fc5ee54aa3cab9945c
Evgeniy Tokarev <hokal89@gmail.com>
2022-09-07 12:45:18 +0700

    First commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 100;
+        int money = 20;
+        int bonus = amount / money;
+        System.out.println("Бонусные мили: " + bonus);
+    }
+}
